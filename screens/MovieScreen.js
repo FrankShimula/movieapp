02054185641a7475ab7249f53934ae3b786ef48f
Frank Shimula <josephmuller53@gmail.com>
2024-01-24 22:21:39 +0300

    fucking lefticon

--- screens/MovieScreen.js
+++ screens/MovieScreen.js
@@ -1,10 +1,29 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView, TouchableOpacity } from 'react-native';
+import { useRoute } from '@react-navigation/native';
+import React, { useEffect } from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { ChevronLeftIcon } from 'react-native-heroicons/outline';
+import { styles } from '../theme';
+
 
 export default function MovieScreen() {
+    const { params: item } = useRoute();
+    useEffect(() => {
+
+    }, [item])
+
     return (
-        <View>
-            <Text>MovieScreen</Text>
-        </View>
+        <ScrollView
+            contentContainerStyle={{ paddingBottom: 20 }}
+            className="flex-1 bg-neutral-900"
+        >
+            <View className="w-full">
+                <SafeAreaView className="absolute z-20 w-full flex-row justify-between items-center px-4 ">
+                    <TouchableOpacity className="rounded-xl p-1">
+                        <ChevronLeftIcon size="28" strokeWidth={2.5} color="white" />
+                    </TouchableOpacity>
+                </SafeAreaView>
+            </View>
+        </ScrollView>
     )
 }
